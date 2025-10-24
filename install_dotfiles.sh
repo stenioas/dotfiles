af7c2c55de945c016ff17888d98a8461f5da5c66
Stenio Silveira <stenio.silveira@softplan.com.br>
2025-10-24 15:32:13 -0300

    feat: add script install_dotfiles e rofi config

---
+++ install_dotfiles.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+DOTFILES_DIR=~/.dotfiles
+CONFIG_DIR=~/.config
+
+declare -A DOTFILES
+DOTFILES=(
+    ["hypr"]="$CONFIG_DIR/hypr"
+    ["rofi"]="$CONFIG_DIR/rofi"
+    # ["waybar"]="$CONFIG_DIR/waybar"
+    # ["kitty"]="$CONFIG_DIR/kitty"
+    # ["dunst"]="$CONFIG_DIR/dunst"
+)
+
+declare -A HOME_FILES
+HOME_FILES=(
+    [".bashrc"]="$HOME/.bashrc"
+)
+
+echo "Setting up dotfiles..."
+
+for folder in "${!DOTFILES[@]}"; do
+    TARGET="${DOTFILES[$folder]}"
+    SOURCE="$DOTFILES_DIR/$folder"
+
+    rm -rf "$TARGET"
+
+    ln -sfn "$SOURCE" "$TARGET"
+    echo "Linked $SOURCE -> $TARGET"
+done
+
+for file in "${!HOME_FILES[@]}"; do
+    SOURCE="$DOTFILES_DIR/$file"
+
+    rm "$HOME/$file"
+    ln -sf "$SOURCE" "$HOME"
+    echo "Copied $SOURCE -> $HOME"
+done
+
+hyprctl reload
+
+echo "Dotfiles setup complete!"
