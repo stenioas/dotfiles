4af8fc0b16d177ed24d2103cb4ab74bcffd601d4
Stenio Silveira <stenioas@gmail.com.br>
2025-11-20 20:11:12 -0300

    refactor(waybar): adjusts checkupdates script

--- waybar/scripts/checkupdates.sh
+++ waybar/scripts/checkupdates.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
 
-# Define a cor de fundo com base no número de atualizações (opcional no Waybar, mas útil para o CSS)
-# Pacman: checa os pacotes e conta as linhas
-UPDATES=$(pacman -Qu | wc -l)
+# needs pacman-contrib package
+UPDATES=$(checkupdates | wc -l)
 
-# Se for maior que zero, exibe a contagem
 if [ "$UPDATES" -gt 0 ]; then
     echo "{\"text\": \"$UPDATES\", \"tooltip\": \"$UPDATES pacotes para atualizar.\", \"class\": \"pending\"}"
-# Se for zero, exibe zero
 else
     echo "{\"text\": \"$UPDATES\", \"tooltip\": \"Sistema atualizado.\", \"class\": \"ready\"}"
 fi
